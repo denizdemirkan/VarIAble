c9d17d3d05278dfa09fa653cf8725a0944d36956
denizdemirkan <denizdemirkan.code@gmail.com>
2023-08-26 23:16:12 +0300

    Refactor: Better Class Structures
    Better Class Structures

---
+++ VerIAble.UI/HelperClasses/StringFieldFilter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerIAble.UI.HelperClasses
+{
+    public class StringFieldFilter
+    {
+        // [/*...*/] --> will return & format string with the value of ... field
+        // ie. [/*FirstName*/].[/*LaseName*/].abc will return like "deniz.demirkan.abc"
+        static string fillWithFields()
+        {
+            return "";
+        }
+    }
+}
